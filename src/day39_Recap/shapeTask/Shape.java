b2584b2b147677a2e94d99d9a58d7b1cc092fe67
Pakdil <eaasbb@gmail.com>
2022-08-06 13:10:28 +0300

    day39_Recap.shapeTask

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -32,9 +32,18 @@ public class Shape {
         return 0;
     }
 
-    public double primeter() {
+    public double perimeter() {
 
         return 0;
     }
 
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
