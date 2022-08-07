18d1a033ebc2e2e6e6943571f11e56988efe46ec
Pakdil <eaasbb@gmail.com>
2022-08-07 10:56:27 +0300

    day40_Access Modifiers, Final Keyword

---
+++ src/utilities/AccessModifiers_Test4.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import day40_FinalKeyward.ProtectedAccessModifier;
+
+public class AccessModifiers_Test4 extends ProtectedAccessModifier {
+
+    public static void main(String[] args) {
+
+        // System.out.println(ProtectedAccessModifier.name1);
+        System.out.println(ProtectedAccessModifier.name2);
+
+
+// AccessModifiers_Test4.method1(); // not visible
+
+AccessModifiers_Test4.method2();
+
+
+
+
+
+
+    }
+
+
+
+}
